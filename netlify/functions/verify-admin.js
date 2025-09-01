50f44f02e11ea895611c256a8c362bcccc4e51a4
iandurst <iandurst2007@gmail.com>
2025-09-01 16:31:04 -0500

    Delete netlify directory

--- netlify/functions/verify-admin.js
+++
@@ -1,6 +0,0 @@
-
-const { ok, bad, notAllowed, requireAdmin } = require('./util');
-exports.handler = async (event) => {
-  if(event.httpMethod !== 'POST') return notAllowed();
-  try{ requireAdmin(event); return ok({ ok:true }) }catch{ return bad('Unauthorized', 401) }
-};

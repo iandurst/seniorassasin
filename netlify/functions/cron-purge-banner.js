8b19984dfe6683363da8e8540a1b35144b1e627e
iandurst <iandurst2007@gmail.com>
2025-09-02 12:36:56 -0500

    Delete netlify/functions directory

--- netlify/functions/cron-purge-banner.js
+++
@@ -1,8 +0,0 @@
-
-const { ok } = require('./_common')
-
-exports.handler = async () => {
-  // This scheduled function placeholder exists to demonstrate Netlify's cron setup.
-  // You can optionally compute & pin purgeStart/purgeEnd here if desired.
-  return ok({ message: 'cron checked' })
-}

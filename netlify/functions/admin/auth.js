1953cb49d9b0b6242a018b41e97cc5980c57e132
iandurst <iandurst2007@gmail.com>
2025-09-02 14:35:38 -0500

    Delete netlify/functions directory

--- netlify/functions/admin/auth.js
+++
@@ -1,8 +0,0 @@
-
-const { ok, bad, requireAdmin } = require('../_common/utils')
-
-exports.handler = async (event) => {
-  // Any method: check header and return 200/401
-  if (!requireAdmin(event)) return bad('Unauthorized', 401)
-  return ok({ ok: true })
-}

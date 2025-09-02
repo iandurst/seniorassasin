2a3721050cc425be49ce0125ecfeebc193b26d8b
iandurst <iandurst2007@gmail.com>
2025-09-02 12:37:02 -0500

    Delete src directory

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react'
-
-export default function Logo({ size = 28 }) {
-  return (
-    <img src="/logo.svg" width={size} height={size} alt="PCS Senior Assassin logo" className="rounded-md" />
-  )
-}

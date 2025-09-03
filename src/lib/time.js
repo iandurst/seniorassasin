f6d03de9eabf368342900c29549e55792aedee5a
iandurst <iandurst2007@gmail.com>
2025-09-03 14:27:38 -0500

    Delete src directory

--- src/lib/time.js
+++
@@ -1,7 +0,0 @@
-
-import { DateTime } from 'luxon'
-export const TZ = 'America/Chicago'
-export const nowChi = () => DateTime.now().setZone(TZ)
-export const isSaturdayPurgeActive = (dt = nowChi()) => dt.weekday === 6 && (dt.hour + dt.minute/60) >= 8 && (dt.hour + dt.minute/60) < 24
-export const isSundayVotingOpen = (dt = nowChi()) => dt.weekday === 7
-export const weekLabel = (dt = nowChi()) => `W${dt.weekNumber} • ${dt.weekYear}`
